a92fe210c18ff744b22e98c49e94aa526486d1e7
Andrew Dao <daoanhphuc84@gmail.com>
2024-10-29 02:00:37 +0700

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Dao Anh Phuc on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
